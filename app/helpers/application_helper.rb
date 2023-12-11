8ee9ec64523a8ba51c990662124631eb8e38bb49
Jesus Eduardo <jesus.cervh13@gmail.com>
2023-12-11 08:33:24 -0600

    Barra de navegacion actualizada Vista agregada en Ecoturismo y Talleres

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def nav_link_class(link_path)
+        current_page?(link_path) ? 'active' : ''
+    end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,19 @@ import "jquery"
 import "jquery_ujs"
 import "popper"
 import "bootstrap"
+
+// JavaScript para rotar el texto
+const textRotator = document.querySelector('.text-rotator');
+const textItems = textRotator.querySelectorAll('.text-img');
+let currentIndex = 0;
+
+function rotateText() {
+    textItems.forEach(item => {
+        item.style.display = 'none';
+    });
+    
+    textItems[currentIndex].style.display = 'block';
+    currentIndex = (currentIndex + 1) % textItems.length;
+}
+
+setInterval(rotateText, 3000); // Cambia cada 3 segundos, ajusta este valor según tu preferencia
